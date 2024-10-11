aae9286bb50dd6ea48ba1a9b7385cba118a8ea78
Nathan876 <sequins.trancheur0c@icloud.com>
2024-10-11 14:27:38 +0200

    Version  1 qui fonctionne sokoban.h

--- sokoban.h
+++ sokoban.h
@@ -4,9 +4,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-/* board.c*/
-char ** init_board(void);
+struct Points {
+    int x;
+    int y;
+};
+
+/* board.c */
+char ** init_board(struct Points character, struct Points box, struct Points target);
 void display_board(char **board);
+
+/* character.c */
+char **mouvement(char **board, struct Points *coordinates, struct Points *box, struct Points *target);
+
+/* verification.c */
+int verification(struct Points *character, struct Points *box, struct Points *target);
 
 #endif /* sokoban_h */
