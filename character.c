54cc1e8d97c25aeb038ada74cf389d5180e61c6a
Nathan876 <sequins.trancheur0c@icloud.com>
2024-10-11 20:31:43 +0200

    Version finale character.c

--- character.c
+++ character.c
@@ -5,10 +5,10 @@ char **mouvement(char **board, struct Points *coordinates, struct Points *box, s
     printf("Choisissez un mouvement : haut(w) bas(s) gauche(a) droite(d)\n");
     scanf(" %c", &coup);
     
-    int new_x = coordinates->x;
+    int new_x = coordinates->x; //Stocke temporairement les coordoonées actuels du personnage
     int new_y = coordinates->y;
     
-    if (coup == 'w') {
+    if (coup == 'w') { // En fonction de la réponse saisie le personnage avance en haut, en bas, à gauche ou à droite
         new_x--;
     } else if (coup == 's') {
         new_x++;
@@ -19,16 +19,16 @@ char **mouvement(char **board, struct Points *coordinates, struct Points *box, s
     } else {
         return board;
     }
-    if (new_x == box->x && new_y == box->y) {
-        int new_box_x = box->x + (new_x - coordinates->x);
+    if (new_x == box->x && new_y == box->y) { // Si les coordonnées du personnage déplacé correspondent aux coordonnées de la boite, la boite est déplacée
+        int new_box_x = box->x + (new_x - coordinates->x); // Initialisation de variables temporaires
         int new_box_y = box->y + (new_y - coordinates->y);
         
-        if (board[new_box_x][new_box_y] == ' ' || (new_box_x == target->x && new_box_y == target->y)) {
+        if (board[new_box_x][new_box_y] == ' ' || (new_box_x == target->x && new_box_y == target->y)) { // Vérifie si la boite peut-être déplacée
             board[box->x][box->y] = ' ';
             box->x = new_box_x;
             box->y = new_box_y;
             board[box->x][box->y] = 'X';
-        } else {
+        } else { // Sinon le plateau est retourné
             return board;
         }
     }
